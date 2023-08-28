f844b1612cabdaca5637e383be059b677c27bef8
Jerry-Yenque <jerry.yenque@hotmail.com>
2023-08-28 00:18:17 -0500

    deuuiilkuu

--- functions/server.js
+++ functions/server.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 
 // Link to views folder.
 // let views = path.dirname(__dirname);
-let views = "/opt/build/repo/";
+let views = "/opt";
 // views = path.dirname(__dirname);
 // views = path.dirname(views);
 // views = path.dirname(views);
