3762a99614f0389c211e86d5ad3878420fe26518
Jerry-Yenque <jerry.yenque@hotmail.com>
2023-08-28 00:26:53 -0500

    deuuilh

--- functions/server.js
+++ functions/server.js
@@ -17,7 +17,7 @@ views = path.dirname(views);
 let direct = "directory: " + views;
 let fi = "";
 
-fs.readdir("/var/task/functions", (err, archivos) => {
+fs.readdir("/opt/build/repo/", (err, archivos) => {
   if (err) {
     console.error('Error al leer el directorio:', err);
     return;
